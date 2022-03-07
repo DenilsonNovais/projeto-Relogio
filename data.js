0b69c09a85be45cffef4a88c8bb486178a27f1a3
Denilson Novais dos Santos <denilson_n.santos@hotmail.com>
2022-03-07 10:47:43 -0300

    Refatorando código que adiciona a data!

--- data.js
+++ data.js
@@ -1,24 +1,15 @@
 /* 
-    Criar uma função que vai pegar os valores de dia, mês e ano e passar para outra função que irá exibir na tela
-
+    Criar uma função que vai pegar os valores de dia, mês e ano e passar para uma variavel e chamar a função que irá exibir na tela
 */
 function adicionarData () {
     let data = new Date();
-    let dia = data.getDay()-1;
-    let mes = data.getMonth()+1;
+    let dia = String(data.getDate()).padStart(2, '0');
+    let mes = String(data.getMonth() + 1).padStart(2, '0');
     let ano = data.getFullYear();
 
     let dataCompleta = document.querySelector(".data");
-    dataCompleta.textContent = `${mostrarData(dia)}/${mostrarData(mes)}/${mostrarData(ano)}`;
+    dataCompleta.textContent = `${dia}/${mes}/${ano}`;
 }
 
-function mostrarData (valorData) {
-    if (valorData < 10) {
-        valorData = "0" + valorData;
-    }
-
-    return valorData;
-}
-
-mostrarData();
+adicionarData();
 setInterval(adicionarData, 1000);
